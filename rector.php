1a34760b992ffbd772958e5bfcda4d80ce05cb30
datashaman <marlinf@datashaman.com>
2022-04-05 08:13:49 +0200

    Use config for skipping rule instead

--- rector.php
+++ rector.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 use Rector\Core\Configuration\Option;
 use Rector\Laravel\Set\LaravelSetList;
+use Rector\Php70\Rector\StaticCall\StaticCallOnNonStaticToInstanceCallRector;
 use Rector\Set\ValueObject\LevelSetList;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
-    // get parameters
     $parameters = $containerConfigurator->parameters();
+
     $parameters->set(Option::PATHS, [
         __DIR__ . '/config',
         __DIR__ . '/src',
@@ -17,7 +18,12 @@ return static function (ContainerConfigurator $containerConfigurator): void {
         __DIR__ . '/views',
     ]);
 
-    // Define what rule sets will be applied
+    $parameters->set(Option::SKIP, [
+        StaticCallOnNonStaticToInstanceCallRector::class => [
+            __DIR__ . '/tests/ShareTest.php',
+        ],
+    ]);
+
     $containerConfigurator->import(LaravelSetList::LARAVEL_60);
     $containerConfigurator->import(LevelSetList::UP_TO_PHP_74);
 };
